93a4cb293f75e97e85d06407e75f97297c2a29e2
allamurodxakimov <xakimov2806@gmail.com>
2023-10-27 21:40:53 -0700

    get email

--- get_email.py
+++ get_email.py
@@ -1,4 +1,4 @@
-import get_data
+import json
 
 def get_email(data:dict) -> list:
     """
@@ -9,3 +9,11 @@ def get_email(data:dict) -> list:
     Returns:
         list: users email
     """
+    func=open(data,'r',encoding="UTF8").read()
+    ls=json.loads(func)["results"]
+    l=[]
+    for i in ls:
+        l.append(i["email"])
+    return l
+data = 'randomuser_data.json'
+print(get_email(data))
