2241cfaf3ef526bbda26338657551a5ae48b0ae0
allamurodxakimov <xakimov2806@gmail.com>
2023-10-27 21:41:50 -0700

    get gender user

--- get_gender_users.py
+++ get_gender_users.py
@@ -1,4 +1,4 @@
-import get_data
+import json
 
 def get_gender_users(data:dict) -> list:
     """
@@ -7,9 +7,22 @@ def get_gender_users(data:dict) -> list:
     The list items are as follows:
         If the user is male: {"Male":1}
         If the user is female: {"Female":0}
-    
     Args:
         data(dict): data
     Returns:
         list: users get gender list
     """
+    c={}
+    f=open(data,'r',encoding="UTF8").read()
+    ls=json.loads(f)['results']
+    l=[]
+    for i in ls:
+        if i["gender"]=="male":
+            c["Male"]=1
+            l.append(c)
+        elif i["gender"]=="female":
+            c["Female"]=0
+            l.append(c)
+    return l
+data="randomuser_data.json"
+print(get_gender_users(data))
