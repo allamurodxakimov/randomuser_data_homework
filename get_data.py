a8135ec7c9af0a32febc4b6b537be87cc0cefc5a
allamurodxakimov <xakimov2806@gmail.com>
2023-10-27 21:40:14 -0700

    get data

--- get_data.py
+++ get_data.py
@@ -1,5 +1,4 @@
-import json
-
+import json 
 def get_data(filename:str) -> dict:
     """
     You are given a filename. Read the JSON data from the file and return the dictionary.
@@ -9,3 +8,8 @@ def get_data(filename:str) -> dict:
     Returns:
         dict: JSON data
     """
+    fun = open(filename,"r",encoding="UTF8").read()
+    ls = json.loads(fun)
+    return ls
+data="randomuser_data.json"
+print(get_data(data))
